99ab4ba569620f78909662921e76923e6dee3856
Manasseh <believemanasseh@gmail.com>
2024-07-21 15:57:37 +0100

    refactor: modify home window class

--- src/windows/home.py
+++ src/windows/home.py
@@ -8,9 +8,16 @@ import gi
 gi.require_version("Gtk", "4.0")
 
 
-from gi.repository import Gdk, Gtk
+from gi.repository import Gtk, Pango
 
-from utils.common import add_style_context, add_style_contexts, load_css, load_image
+from utils.common import (
+	add_style_context,
+	add_style_contexts,
+	append_all,
+	create_cursor,
+	load_css,
+	load_image,
+)
 
 
 class HomeWindow:
@@ -20,6 +27,7 @@ class HomeWindow:
 		"""Maximises base application window and styles base box widget."""
 		self.base = base_window
 		self.box = base_box
+		self.cursor = create_cursor("pointer")
 		self.css_provider = load_css("/assets/styles/home.css")
 		self.box.set_valign(Gtk.Align.START)
 		add_style_context(self.box, self.css_provider)
@@ -33,131 +41,161 @@ class HomeWindow:
 			spacing=10,
 		)
 		add_style_context(post_container, self.css_provider)
+
 		self.box.append(post_container)
 
-		# Add likes count and upvote/downvote buttons to navigation box
-		navigation_box = Gtk.Box(
-			orientation=Gtk.Orientation.VERTICAL, spacing=10, css_classes=["icon-box"]
-		)
-		add_style_context(navigation_box, self.css_provider)
+		vote_btns_box = self.__add_vote_buttons()
+		post_container.append(vote_btns_box)
 
-		upvote_btn = Gtk.Button(icon_name="upvote-btn")
-		navigation_box.append(upvote_btn)
+		post_image_box = self.__add_post_image()
+		post_container.append(post_image_box)
 
-		likes_count = Gtk.Label(label=6000, css_classes=["likes-count"])
-		add_style_context(likes_count, self.css_provider)
-		navigation_box.append(likes_count)
+		post_metadata_box = self.__add_post_metadata()
+		post_container.append(post_metadata_box)
 
-		downvote_btn = Gtk.Button(icon_name="downvote-btn")
-		navigation_box.append(downvote_btn)
+	def __add_post_image(self) -> Gtk.Box:
+		"""Add post image."""
+		post_image_box = Gtk.Box(css_classes=["post-image-box"], height_request=100)
+		add_style_context(post_image_box, self.css_provider)
 
-		post_container.append(navigation_box)
-
-		# Add image to post container
-		post_image_box = Gtk.Box(
-			css_classes=["post-image-box"],
-		)
 		post_image = load_image(
 			"/assets/images/light.jpg", "Light", ["post-image"], self.css_provider
 		)
 		post_image_box.append(post_image)
 
-		post_container.append(post_image_box)
+		return post_image_box
 
-		# Add post title and necessary metadata
-		# Metadata includes post title, post user, post subreddit, post
-		# submission date etc
-		post_details_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-		cursor = Gdk.Cursor.new_from_name("pointer")
+	def __add_vote_buttons(self) -> Gtk.Box:
+		"""Add likes count and upvote/downvote buttons."""
+		box = Gtk.Box(
+			orientation=Gtk.Orientation.VERTICAL, spacing=10, css_classes=["icon-box"]
+		)
+		add_style_context(box, self.css_provider)
+
+		upvote_btn = Gtk.Button(icon_name="upvote-btn", cursor=self.cursor)
+		box.append(upvote_btn)
+
+		likes_count = Gtk.Label(label=6000, css_classes=["likes-count"])
+		add_style_context(likes_count, self.css_provider)
+		box.append(likes_count)
+
+		downvote_btn = Gtk.Button(icon_name="downvote-btn", cursor=self.cursor)
+		box.append(downvote_btn)
+
+		return box
+
+	def __add_post_metadata(self) -> Gtk.Box:
+		"""Add widgets for post metadata (e.g. post title, post user, post subreddit)."""
+		post_metadata_box = Gtk.Box(
+			css_classes=["post-metadata-box"],
+			orientation=Gtk.Orientation.VERTICAL,
+			valign=Gtk.Align.CENTER,
+		)
+		add_style_context(post_metadata_box, self.css_provider)
+
 		post_title = Gtk.Label(
-			label="This is the first post from the Reddit API client and whatever the fuck that means I don't give a fuck mans fuck me.",
+			label="This is the first post from the Reddit API client and whatever the #",
 			css_classes=["post-title"],
 			wrap=True,
-			valign=Gtk.Align.START,
-			cursor=cursor,
+			hexpand=True,
+			wrap_mode=Pango.WrapMode.CHAR,
+			cursor=self.cursor,
 		)
 		add_style_context(post_title, self.css_provider)
-		post_details_box.append(post_title)
-
-		post_metadata_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-		post_metadata = Gtk.Label(
+		post_metadata_box.append(post_title)
+		post_box = Gtk.Box(margin_top=5, orientation=Gtk.Orientation.HORIZONTAL)
+		post_time = Gtk.Label(
 			label="submitted 23 hours ago by ",
 			css_classes=["post-metadata"],
 			halign=Gtk.Align.START,
 			margin_top=5,
 		)
+
 		post_user = Gtk.Label(
-			label="FriendlyBabyFrog ",
+			label="<u>FriendlyBabyFrog</u> ",
 			css_classes=["post-user"],
-			cursor=cursor,
+			cursor=self.cursor,
+			use_markup=True,
 			margin_top=5,
 		)
 		add_style_context(post_user, self.css_provider)
-		post_metadata2 = Gtk.Label(label="to ", margin_top=5)
+
+		post_text = Gtk.Label(label="to ", css_classes=["post-metadata"], margin_top=5)
+		add_style_contexts([post_time, post_text], self.css_provider)
+
 		post_subreddit = Gtk.Label(
-			label="r/mildlyinteresting",
+			label="<u>r/mildlyinteresting</u>",
 			css_classes=["post-subreddit"],
-			cursor=cursor,
+			cursor=self.cursor,
+			use_markup=True,
 			margin_top=5,
 		)
 		add_style_context(post_subreddit, self.css_provider)
-		post_metadata_box.append(post_metadata)
-		post_metadata_box.append(post_user)
-		post_metadata_box.append(post_metadata2)
-		post_metadata_box.append(post_subreddit)
 
+		append_all(post_box, [post_time, post_user, post_text, post_subreddit])
+
+		post_metadata_box.append(post_box)
+
+		post_action_btns_box = self.__add_action_btns()
+
+		post_metadata_box.append(post_action_btns_box)
+
+		return post_metadata_box
+
+	def __add_action_btns(self) -> Gtk.Box:
+		"""Add widgets for action buttons (e.g. share, save, crosspost, etc)."""
 		post_action_btns_box = Gtk.Box(
 			orientation=Gtk.Orientation.HORIZONTAL,
 			halign=Gtk.Align.START,
-			css_classes=["post-action-btns"],
+			margin_top=5,
 		)
 		post_comments = Gtk.Label(
+			label="<u>3274 comments</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_comments.set_text_with_mnemonic("_3274 comments_ ")
 
 		post_share = Gtk.Label(
+			label="<u>share</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_share.set_text_with_mnemonic("_share_ ")
 
 		post_save = Gtk.Label(
+			label="<u>save</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_save.set_text_with_mnemonic("_save_ ")
 
 		post_hide = Gtk.Label(
+			label="<u>hide</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_hide.set_text_with_mnemonic("_hide_ ")
 
 		post_report = Gtk.Label(
+			label="<u>report</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_report.set_text_with_mnemonic("_report_ ")
 
 		post_crosspost = Gtk.Label(
+			label="<u>crosspost</u> ",
 			use_markup=True,
 			css_classes=["post-action-btn"],
-			cursor=cursor,
+			cursor=self.cursor,
 			margin_top=5,
 		)
-		post_crosspost.set_text_with_mnemonic("_crosspost_ ")
 
 		add_style_contexts(
 			[
@@ -177,7 +215,4 @@ class HomeWindow:
 		post_action_btns_box.append(post_report)
 		post_action_btns_box.append(post_crosspost)
 
-		post_details_box.append(post_metadata)
-		post_details_box.append(post_metadata_box)
-		post_container.append(post_details_box)
-		post_container.append(post_action_btns_box)
+		return post_action_btns_box
