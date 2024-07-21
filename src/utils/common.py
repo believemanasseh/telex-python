52b71cae260a3c5c96f9cbb1914f42bb34d33460
Manasseh <believemanasseh@gmail.com>
2024-07-21 02:31:21 +0100

    feat: add post metadata

--- src/utils/common.py
+++ src/utils/common.py
@@ -28,3 +28,18 @@ def load_css(css_path) -> Gtk.CssProvider:
 	abspath = os.path.abspath(__file__)
 	css_provider.load_from_path(abspath[: len(abspath) - 16] + css_path)
 	return css_provider
+
+
+def add_style_context(widget: Gtk.Widget, css_provider: Gtk.CssProvider) -> None:
+	"""Add css style context to widget."""
+	widget.get_style_context().add_provider(
+		css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+	)
+
+
+def add_style_contexts(widgets: list[Gtk.Widget], css_provider: Gtk.CssProvider) -> None:
+	"""Add multiple css style contexts to widget."""
+	for widget in widgets:
+		widget.get_style_context().add_provider(
+			css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+		)
--- src/windows/home.py
+++ src/windows/home.py
@@ -8,9 +8,9 @@ import gi
 gi.require_version("Gtk", "4.0")
 
 
-from gi.repository import Gtk
+from gi.repository import Gdk, Gtk
 
-from utils.common import load_css, load_image
+from utils.common import add_style_context, add_style_contexts, load_css, load_image
 
 
 class HomeWindow:
@@ -22,9 +22,7 @@ class HomeWindow:
 		self.box = base_box
 		self.css_provider = load_css("/assets/styles/home.css")
 		self.box.set_valign(Gtk.Align.START)
-		self.box.get_style_context().add_provider(
-			self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-		)
+		add_style_context(self.box, self.css_provider)
 		self.base.maximize()
 
 	def render_page(self):
@@ -34,26 +32,20 @@ class HomeWindow:
 			orientation=Gtk.Orientation.HORIZONTAL,
 			spacing=10,
 		)
-		post_container.get_style_context().add_provider(
-			self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-		)
+		add_style_context(post_container, self.css_provider)
 		self.box.append(post_container)
 
-		# Add upvote and downvote buttons to navigation box
+		# Add likes count and upvote/downvote buttons to navigation box
 		navigation_box = Gtk.Box(
 			orientation=Gtk.Orientation.VERTICAL, spacing=10, css_classes=["icon-box"]
 		)
-		navigation_box.get_style_context().add_provider(
-			self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-		)
+		add_style_context(navigation_box, self.css_provider)
 
 		upvote_btn = Gtk.Button(icon_name="upvote-btn")
 		navigation_box.append(upvote_btn)
 
 		likes_count = Gtk.Label(label=6000, css_classes=["likes-count"])
-		likes_count.get_style_context().add_provider(
-			self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-		)
+		add_style_context(likes_count, self.css_provider)
 		navigation_box.append(likes_count)
 
 		downvote_btn = Gtk.Button(icon_name="downvote-btn")
@@ -73,10 +65,119 @@ class HomeWindow:
 		post_container.append(post_image_box)
 
 		# Add post title and necessary metadata
-		post_detail_box = Gtk.Box(css_classes=["post-detail-box"])
-		post_title = Gtk.Text(text="Post Title", css_classes=["post-title"])
-		post_title.get_style_context().add_provider(
-			self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+		# Metadata includes post title, post user, post subreddit, post
+		# submission date etc
+		post_details_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+		cursor = Gdk.Cursor.new_from_name("pointer")
+		post_title = Gtk.Label(
+			label="This is the first post from the Reddit API client and whatever the fuck that means I don't give a fuck mans fuck me.",
+			css_classes=["post-title"],
+			wrap=True,
+			valign=Gtk.Align.START,
+			cursor=cursor,
 		)
-		post_detail_box.append(post_title)
-		post_container.append(post_detail_box)
+		add_style_context(post_title, self.css_provider)
+		post_details_box.append(post_title)
+
+		post_metadata_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+		post_metadata = Gtk.Label(
+			label="submitted 23 hours ago by ",
+			css_classes=["post-metadata"],
+			halign=Gtk.Align.START,
+			margin_top=5,
+		)
+		post_user = Gtk.Label(
+			label="FriendlyBabyFrog ",
+			css_classes=["post-user"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		add_style_context(post_user, self.css_provider)
+		post_metadata2 = Gtk.Label(label="to ", margin_top=5)
+		post_subreddit = Gtk.Label(
+			label="r/mildlyinteresting",
+			css_classes=["post-subreddit"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		add_style_context(post_subreddit, self.css_provider)
+		post_metadata_box.append(post_metadata)
+		post_metadata_box.append(post_user)
+		post_metadata_box.append(post_metadata2)
+		post_metadata_box.append(post_subreddit)
+
+		post_action_btns_box = Gtk.Box(
+			orientation=Gtk.Orientation.HORIZONTAL,
+			halign=Gtk.Align.START,
+			css_classes=["post-action-btns"],
+		)
+		post_comments = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_comments.set_text_with_mnemonic("_3274 comments_ ")
+
+		post_share = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_share.set_text_with_mnemonic("_share_ ")
+
+		post_save = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_save.set_text_with_mnemonic("_save_ ")
+
+		post_hide = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_hide.set_text_with_mnemonic("_hide_ ")
+
+		post_report = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_report.set_text_with_mnemonic("_report_ ")
+
+		post_crosspost = Gtk.Label(
+			use_markup=True,
+			css_classes=["post-action-btn"],
+			cursor=cursor,
+			margin_top=5,
+		)
+		post_crosspost.set_text_with_mnemonic("_crosspost_ ")
+
+		add_style_contexts(
+			[
+				post_comments,
+				post_share,
+				post_save,
+				post_hide,
+				post_report,
+				post_crosspost,
+			],
+			self.css_provider,
+		)
+		post_action_btns_box.append(post_comments)
+		post_action_btns_box.append(post_share)
+		post_action_btns_box.append(post_save)
+		post_action_btns_box.append(post_hide)
+		post_action_btns_box.append(post_report)
+		post_action_btns_box.append(post_crosspost)
+
+		post_details_box.append(post_metadata)
+		post_details_box.append(post_metadata_box)
+		post_container.append(post_details_box)
+		post_container.append(post_action_btns_box)
