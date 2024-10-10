1abc0e58b04e04b328bf5bd2b4733c3cea750cbe
Manasseh <believemanasseh@gmail.com>
2024-10-10 22:11:43 +0100

    fix: resolve import bug

--- src/windows/home.py
+++ src/windows/home.py
@@ -10,7 +10,7 @@ gi.require_version("Gtk", "4.0")
 
 from gi.repository import Gtk, Pango
 
-from utils.commo import (
+from utils.common import (
 	add_style_context,
 	add_style_contexts,
 	append_all,
