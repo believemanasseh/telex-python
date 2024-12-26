2125c2b03a34e73be35773edabfc7b522e899e04
Manasseh <believemanasseh@gmail.com>
2024-12-26 14:28:19 +0100

    chore: modify build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 sudo rm -r build
 pip install -r requirements.txt
-sudo meson setup --reconfigure build
-sudo meson compile --clean -C build
-sudo meson install -C build
-cd /usr/local/bin && ./telex
+meson setup --reconfigure build
+meson compile --clean -C build
+meson install -C build
+cd ./build/src && ./telex
