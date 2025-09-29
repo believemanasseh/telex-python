05478fc130047880ce292b265e6b8599d21808e8
Manasseh <believemanasseh@gmail.com>
2025-09-29 21:54:23 +0100

    feat: add widgets for creating text post, links and uploading media

--- src/windows/new_post.py
+++ src/windows/new_post.py
@@ -1,7 +1,7 @@
-"""New post window module for the Telex application.
+"""New post dialog module for the Telex application.
 
 This module provides:
-- NewPostWindow: Main window for creating new Reddit post.
+- NewPostDialog: Main dialog for creating new Reddit post.
 """
 
 import gi
@@ -16,10 +16,10 @@ MAX_CHAR = 300
 
 
 class NewPostDialog(Adw.Dialog):
-	"""New Post window for Telex application."""
+	"""New Post dialog for Telex application."""
 
 	def __init__(self, **kwargs):
-		"""Initialise the New Post window."""
+		"""Initialise the New Post dialog."""
 		super().__init__(**kwargs)
 
 		self.css_provider = load_css("/assets/styles/new_post.css")
@@ -37,11 +37,23 @@ class NewPostDialog(Adw.Dialog):
 			child_box = Gtk.Box(
 				halign=Gtk.Align.CENTER,
 				orientation=Gtk.Orientation.VERTICAL,
-				margin_bottom=0,
+				margin_bottom=30,
+				vexpand=True,
 			)
+			box = Gtk.Box(margin_top=20)
+			title_label = Gtk.Label(
+				label="Title",
+				halign=Gtk.Align.START,
+				css_classes=["title-label"],
+			)
+			asterisk_label = Gtk.Label(
+				label="*", css_classes=["asterisk-label"], margin_bottom=5
+			)
+			box.append(title_label)
+			box.append(asterisk_label)
 			buffer = Gtk.TextBuffer()
 			buffer.connect("insert-text", self.__on_insert_text)
-			widget = Gtk.TextView(
+			text_widget = Gtk.TextView(
 				buffer=buffer,
 				css_classes=["title-input"],
 				width_request=500,
@@ -52,20 +64,67 @@ class NewPostDialog(Adw.Dialog):
 				right_margin=5,
 				bottom_margin=5,
 			)
-			title_label = Gtk.Label(
-				label="Title:",
-				margin_top=10,
-				halign=Gtk.Align.START,
-				css_classes=["title-label"],
-			)
-			child_box.append(title_label)
-			child_box.append(widget)
+			child_box.append(box)
+			child_box.append(text_widget)
+
+			if label in {"Text", "Link"}:
+				# Add body text and link input field
+				box = Gtk.Box(margin_top=20)
+				body_link_label = Gtk.Label(
+					label="Body text (optional)" if label == "Text" else "Link URL",
+					halign=Gtk.Align.START,
+					css_classes=["body-label"],
+				)
+				box.append(body_link_label)
+				if label == "Link":
+					asterisk_label = Gtk.Label(
+						label="*", css_classes=["asterisk-label"], margin_bottom=5
+					)
+					box.append(asterisk_label)
+
+				buffer = Gtk.TextBuffer()
+
+				body_widget = Gtk.TextView(
+					buffer=buffer,
+					css_classes=["body-input"],
+					width_request=500,
+					wrap_mode=Gtk.WrapMode.CHAR,
+					left_margin=5,
+					top_margin=5,
+					right_margin=5,
+					bottom_margin=5,
+				)
+
+				clamp = Adw.Clamp(
+					child=body_widget,
+					maximum_size=250,
+					orientation=Gtk.Orientation.VERTICAL,
+				)
+				viewport = Gtk.Viewport(child=clamp)
+				scrolled_window = Gtk.ScrolledWindow(
+					child=viewport,
+					hscrollbar_policy=Gtk.PolicyType.NEVER,
+					vscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
+					height_request=250 if label == "Text" else 30,
+				)
+
+				child_box.append(box)
+				child_box.append(scrolled_window)
+			else:
+				parent = Gtk.Window()
+				button = Gtk.Button(label="Choose File…", margin_top=10)
+				button.connect("clicked", self.__on_upload_clicked, parent, child_box)
+				child_box.append(button)
+
 			notebook.append_page(
 				child=child_box,
 				tab_label=Gtk.Label(label=label),
 			)
 
-			add_style_contexts([widget, title_label], self.css_provider)
+			add_style_contexts(
+				[text_widget, title_label, asterisk_label, body_widget, body_link_label],
+				self.css_provider,
+			)
 
 		button = Gtk.Button(
 			icon_name="xyz.daimones.Telex.close",
@@ -86,7 +145,7 @@ class NewPostDialog(Adw.Dialog):
 	):
 		"""Handles insert text events for TextView widget.
 
-		Sets maximum text length that can be stored in the buffer.
+		Limits the number of characters in the title input field to MAX_CHAR.
 
 		Args:
 			text_buffer (Gtk.TextBuffer): The buffer that triggered the event
@@ -99,3 +158,65 @@ class NewPostDialog(Adw.Dialog):
 		"""
 		if text_buffer.get_char_count() >= MAX_CHAR:
 			text_buffer.stop_emission_by_name("insert-text")
+
+	def __on_upload_clicked(
+		self, _button: Gtk.Button, parent: Gtk.Window, child_box: Gtk.Box
+	):
+		"""Handles file upload button click event.
+
+		Opens a file chooser dialog to select a file.
+
+		Args:
+			_button (Gtk.Button): The button that was clicked
+			parent (Gtk.Window): The parent window for the file chooser dialog
+			child_box (Gtk.Box): The box to add the selected image preview
+
+		Returns:
+			None: This method does not return a value.
+		"""
+		dialog = Gtk.FileChooserNative(
+			title="Select a file",
+			transient_for=parent,
+			action=Gtk.FileChooserAction.OPEN,
+			accept_label="_Open",
+			cancel_label="_Cancel",
+		)
+		dialog.connect("response", self.__on_file_dialog_response, child_box)
+		dialog.show()
+
+	def __on_file_dialog_response(
+		self, dialog: Gtk.FileChooserNative, response: int, child_box: Gtk.Box
+	):
+		"""Handles file chooser dialog response event.
+
+		If the user accepted the dialog, retrieves the selected file path and
+		displays a preview of the image in the provided box.
+
+		Args:
+			dialog (Gtk.FileChooserNative): The file chooser dialog
+			response (int): The response ID from the dialog
+			child_box (Gtk.Box): The box to add the selected image preview
+
+		Returns:
+			None: This method does not return a value.
+		"""
+		if response == Gtk.ResponseType.ACCEPT:
+			filename = dialog.get_file().get_path()
+			if filename:
+				picture = Gtk.Picture.new_for_filename(filename)
+				picture.set_content_fit(Gtk.ContentFit.SCALE_DOWN)
+				picture.set_margin_top(10)
+				box = Gtk.Box(
+					orientation=Gtk.Orientation.HORIZONTAL,
+					spacing=6,
+					margin_top=6,
+					margin_bottom=6,
+					margin_start=6,
+					margin_end=6,
+				)
+				box.append(picture)
+				lbr = Gtk.ListBoxRow()
+				lbr.set_child(box)
+				child_box.append(lbr)
+
+		dialog.destroy()
